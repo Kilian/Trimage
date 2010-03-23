6a00b2e25c96fc98239c28947e64f9c2b238b080
Kilian Valkhof <kilian@kilianvalkhof.com>
2010-03-23 14:57:50 +0100

    move runner to own folder

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name = "trimage",
                                  "pixmaps/view-refresh.png",
                                  "pixmaps/compressing.gif",
                                  "pixmaps/trimage-icon.png"])],
-      scripts = ["trimage"],
+      scripts = ["runner/trimage"],
       long_description = """Trimage is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and jpegoptim, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Trimage gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
       requires = ["PyQt4 (>4.4)"],
       classifiers = [
