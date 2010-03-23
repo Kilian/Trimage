9826e6cd970eb70b1dea96892d80259f54e49356
Kilian Valkhof <kilian@kilianvalkhof.com>
2010-03-23 14:34:52 +0100

    more setup work

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from trimage import VERSION
 
 setup(name = "Trimage",
-      version = "1.0.0b",
+      version = VERSION,
       description = "Trimage image compressor - A cross-platform tool for optimizing PNG and JPG files",
       author = "Kilian Valkhof, Paul Chaplin",
       author_email = "help@trimage.org",
@@ -14,7 +15,13 @@ setup(name = "Trimage",
                                  "pixmaps/view-refresh.png",
                                  "pixmaps/compressing.gif",
                                  "pixmaps/trimage-icon.png"])],
-      scripts = ["trimage"],
+      scripts = ["trimage.py"],
       long_description = """Trimage is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and jpegoptim, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Trimage gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
       requires = ["PyQt4 (>4.4)"],
+      classifiers = [
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+      ],
+
       )
