23c3243209fdd86ba5650450bf20832ba918fddd
Hugo Posnic <hugo.posnic@gmail.com>
2017-11-19 17:48:13 +0100

    Remove two remaining "u" chars

--- src/trimage/trimage.py
+++ src/trimage/trimage.py
@@ -429,8 +429,8 @@ class Image:
         self.compressing = True
         exe = ".exe" if (sys.platform == "win32") else ""
         runString = {
-            "jpeg": u"jpegoptim" + exe + " -f --strip-all '%(file)s'",
-            "png": u"optipng" + exe + " -force -o7 '%(file)s'&&advpng" + exe + " -z4 '%(file)s' && pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time '%(file)s' '%(file)s.bak' && mv '%(file)s.bak' '%(file)s'"
+            "jpeg": "jpegoptim" + exe + " -f --strip-all '%(file)s'",
+            "png": "optipng" + exe + " -force -o7 '%(file)s'&&advpng" + exe + " -z4 '%(file)s' && pngcrush -rem gAMA -rem alla -rem cHRM -rem iCCP -rem sRGB -rem time '%(file)s' '%(file)s.bak' && mv '%(file)s.bak' '%(file)s'"
         }
         # Create a backup file
         copy(self.fullpath, self.fullpath + '~')
