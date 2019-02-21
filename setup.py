4e702d3f3a37960d0a1ef1a2f9da6d007a31ec45
Hugo Posnic <hugo.posnic@gmail.com>
2019-02-21 15:34:59 +0100

    Simplify the structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 win=(sys.platform == "win32")
 if win:
   import py2exe
-  sys.path.append("src/trimage")
+  sys.path.append("trimage")
 
 from distutils.core import setup
 
@@ -15,19 +15,18 @@ setup(name = "trimage",
     author_email = "help@trimage.org",
     url = "http://trimage.org",
     license = "MIT license",
-    package_dir = {'trimage' : 'src/trimage'},
     packages = ["trimage",
               "trimage.ThreadPool"],
     package_data = {"trimage" : ["pixmaps/*.*"] },
     data_files=[('share/icons/hicolor/scalable/apps', ['desktop/trimage.svg']),
             ('share/applications', ['desktop/trimage.desktop']),
             ('share/man/man1', ['doc/trimage.1'])],
-    scripts = ["trimage"],
+    scripts = ["bin/trimage"],
     long_description = """Trimage is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and jpegoptim, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Trimage gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
     requires = ["PyQt5"],
 
     #for py2exe
-    windows=[r'src\trimage\trimage.py'],
+    windows=[r'trimage\trimage.py'],
     zipfile=None,
     options={"py2exe":{
         "optimize":2,
