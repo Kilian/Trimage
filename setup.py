d6118b520b2f9fd3ba23b33015cd23b6474064ed
Huluti <hugo.posnic@gmail.com>
2019-03-07 16:09:21 +0100

    Remove Windows options from setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
-win=(sys.platform == "win32")
-if win:
-  import py2exe
-  sys.path.append("trimage")
-
 from distutils.core import setup
+
 
 setup(name = "trimage",
     version = "1.0.5",
@@ -23,17 +18,5 @@ setup(name = "trimage",
             ('share/man/man1', ['doc/trimage.1'])],
     scripts = ["bin/trimage"],
     long_description = """Trimage is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and jpegoptim, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Trimage gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
-    requires = ["PyQt5"],
-
-    #for py2exe
-    windows=[r'trimage\trimage.py'],
-    zipfile=None,
-    options={"py2exe":{
-        "optimize":2,
-        "compressed":1,
-        "bundle_files":1,
-        "includes":["sip",],
-        "excludes":['email'],
-        },
-    },
+    requires = ["PyQt5"]
   )
